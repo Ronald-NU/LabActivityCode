c2ccec2d4c72c133c218bc429b6d31b7c9fafda6
Ronald <mundell.r@northeastern.edu>
2025-01-13 14:25:52 -0800

    refactor Input component to add explicit type annotation for state variable

--- components/Input.tsx
+++ components/Input.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { TextInput, View } from 'react-native';
 
 const Input = () => {
-    const [text, setText] = useState('');
+    const [text, setText] = useState<string>('');
 
     return(
         <View>
