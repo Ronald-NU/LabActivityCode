55986db29eee85938eb8b53772e1e835509ee983
Ronald <mundell.r@northeastern.edu>
2025-01-13 14:11:13 -0800

    refactor Input component to remove explicit type annotation for functional component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { TextInput, View } from 'react-native';
 
-const Input:React.FC = () => {
+const Input = () => {
     const [text, setText] = useState('');
 
     return(
