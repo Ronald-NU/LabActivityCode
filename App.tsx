8ff2b7e49056c84938c2f8d5c9aa1f26af1d3e1c
Ronald <mundell.r@northeastern.edu>
2025-01-13 13:20:00 -0800

    add TextInput component to App for user input that displays on Text component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './components/Header';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+  
   var appName = "Lab Activity Code";
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header appName={appName} />
+      <TextInput placeholder='Enter Text' style={{height:40}}
+      value={text}
+      onChangeText={text => setText(text)}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
