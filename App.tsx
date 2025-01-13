8246a0fb787a67ea331a2e029b421a3c601e51e1
Ronald <mundell.r@northeastern.edu>
2025-01-13 12:43:33 -0800

    update App component to display welcome message with app name

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  var appName = "Lab Activity Code";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Welcome to {appName}</Text>
     </View>
   );
 }
