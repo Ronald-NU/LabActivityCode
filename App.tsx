52de1936e73bb875ff4effa29a77261431ebd4af
Ronald <mundell.r@northeastern.edu>
2025-01-20 13:28:34 -0800

    add modal input for goal entry, toggle visibility with button and update handleInputData to hide modal

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 import { useState } from 'react';
@@ -7,23 +7,26 @@ import { useState } from 'react';
 export default function App() {
 
   const [text, setText] = useState<string>('');
+  const [isInputVisable, setIsInputVisable] = useState<boolean>(false);
   const appName = "Lab Activity Code";
   //controls if the input componenet is focused on render
   const isFocusedOnRender = true;
 
-  //Function to handle input data from the input component
+  //Function to handle input data from the input component and hide modal
   const handleInputData = (data: string) => {
     setText(data)
+    setIsInputVisable(false)
   }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header appName={appName} />
-      <Input focusOnRender={isFocusedOnRender} handleInputData={handleInputData} />
+      <Input isVisable={isInputVisable} focusOnRender={isFocusedOnRender} handleInputData={handleInputData} />
       {
       <Text>{text}</Text>
       }
+      <Button title='Add a goal' onPress={()=>setIsInputVisable(true)}/>
     </View>
   );
 }
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
-import { TextInput, View, Text, Button } from 'react-native';
+import { TextInput, Modal, Text, Button, StyleSheet, View } from 'react-native';
 
 interface InputProps {
     focusOnRender: boolean;
     handleInputData: (data: string) => void;
+    isVisable: boolean;
 }
 
-const Input = ({focusOnRender, handleInputData}: InputProps) => {
+const Input = ({focusOnRender, handleInputData, isVisable}: InputProps) => {
     const [text, setText] = useState<string>('');
     const [isFocused, setIsFocused] = useState<boolean>(focusOnRender);
     const [hasBlurred, setHasBlurred] = useState<boolean>(false);
@@ -40,7 +41,8 @@ const Input = ({focusOnRender, handleInputData}: InputProps) => {
     }
 
     return(
-        <View>
+        <Modal animationType='slide' visible={isVisable}>
+            <View style={styles.container}>
             <TextInput 
                 placeholder='Enter Text' 
                 style={{height:40}} 
@@ -63,8 +65,17 @@ const Input = ({focusOnRender, handleInputData}: InputProps) => {
                 </Text>:null
                 }
                 <Button title="Confirm" onPress={handleConfirm}/>
-            </View>
+                </View>
+            </Modal>
     )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Input;
