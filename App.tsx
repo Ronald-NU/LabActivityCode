11b1b39c92bae084d12134b5eca37c271f8af7d3
Ronald <mundell.r@northeastern.edu>
2025-01-27 14:49:04 -0800

    feat: add item separator component to goal list for improved visual separation

--- App.tsx
+++ App.tsx
@@ -71,6 +71,7 @@ export default function App() {
         ListEmptyComponent={<Text style={styles.text}>No goals to show</Text>}
         ListHeaderComponent={goals.length>0?<Text style={styles.text}>My Goal List</Text>:null}
         ListFooterComponent={goals.length>0?<Button title={'Delete All'} onPress={handelDeleteAll}/>:null}
+        ItemSeparatorComponent={() => <View style={{ height: 3, backgroundColor: '#e0e0e0' }} />}
         />
       </View>
       
