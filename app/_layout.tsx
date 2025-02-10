bc99c2b4e6bab14c2c7ca684919b4979b9d2a468
Ronald <mundell.r@northeastern.edu>
2025-02-10 13:42:29 -0800

    Add goal details screen and update layout for navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="goals/[id]" />
+  </Stack>;
 }
