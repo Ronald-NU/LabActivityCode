42264b434f5bf69b5c0dee7721f9ee35368f12e3
Ronald <mundell.r@northeastern.edu>
2025-02-10 13:57:05 -0800

    Update layout to include customized header styles for goal screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,26 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack/>
-  /*<Stack>
-    <Stack.Screen name="index" />
-    <Stack.Screen name="goals/[id]" />
-  </Stack>*/;
+  return   (
+    <Stack>
+    <Stack.Screen name="index"
+    options={{title:'All My Goals',
+        headerStyle:{
+            backgroundColor:'orange'
+        },
+        headerTitleStyle:{
+            color:'white'
+        }}} />
+    <Stack.Screen 
+    options={{title:'Goal Details',
+        headerStyle:{
+            backgroundColor:'orange'
+        },
+        headerTitleStyle:{
+            color:'white'
+        },
+        headerTintColor:'white'
+    }}
+    name="goals/[id]" />
+    </Stack>)
 }
