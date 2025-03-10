9e04706cbbbeaea2b804fc2b6ec2d312dd6f7cd5
Ronald <mundell.r@northeastern.edu>
2025-03-10 15:02:20 -0700

    Added comment for next steps

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -24,6 +24,7 @@ export default function App() {
   const appName = "Lab Activity Code";
   const isFocusedOnRender = true;
   useEffect(() => {
+    //add where clause to query
     const unsubscribe = onSnapshot(query(collection(database, "goals")), (querySnapshot) => {
       if(!querySnapshot.empty){
         setGoals(goals => []);
