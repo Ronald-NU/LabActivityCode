d2ffcb4575d3a7fd0f556394e70aebce2359ab4f
Ronald <mundell.r@northeastern.edu>
2025-03-10 13:52:55 -0700

    Added observer for firebase users auth state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
+import { auth } from "@/Firebase/firebaseSetup";
 import { Stack } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 export default function Layout() {
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+            setUserLoggedIn(true);
+          } else {
+          // User is signed out
+            setUserLoggedIn(false);
+          }
+        })
+      }, []);
     return   (
     <Stack screenOptions={{headerShown:false}}>
     </Stack>)
