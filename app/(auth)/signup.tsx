aa07e842e12734f2a5cecbeb4b0f7c01a1f18a8c
Ronald <mundell.r@northeastern.edu>
2025-03-10 14:09:24 -0700

    Updated Signup to include registration with email and password

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,12 +1,32 @@
-import { Text, StyleSheet, View, TextInput, Button } from 'react-native'
+import { Text, StyleSheet, View, TextInput, Button, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { router } from 'expo-router';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/Firebase/firebaseSetup';
 
 export default function signup() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmpassword, setConfirmPassword] = useState('');
 
+    const createUser = async () => {
+        if (password === confirmpassword) {
+            try {
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+                const user = userCredential.user;
+                console.log(user);
+                router.replace('./login');
+            } catch (error) {
+                Alert.alert('Could not Register', 
+                    "Make sure to imput a valid email and password with "+ 
+                    "at least 8 characters and 1 captial letter!");
+            }
+        }
+        else {
+            Alert.alert('Could Not Register', 
+                "Make sure the passwords match and have at least 8 characters and 1 captial letter!");
+        }
+    }
     return (
     <View style={styles.container}>
         <Text style={styles.text}>Email Address</Text>
@@ -30,7 +50,7 @@ export default function signup() {
         secureTextEntry={true}
         value={confirmpassword}
         onChangeText={text => {setConfirmPassword(text)}}/>
-        <Button title="Register" onPress={()=>{}}/>
+        <Button title="Register" onPress={()=>{createUser()}}/>
         <Button title="Already Registered? Login" onPress={() => router.replace('./login')}/>
     </View>
     )
