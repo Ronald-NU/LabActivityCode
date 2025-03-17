5e0fcb7708babcf3d2584b1f095ad91dc53886d0
Ronald <mundell.r@northeastern.edu>
2025-03-17 13:32:34 -0700

    Added VerifyImagePermissions which asks for permissions or just returns true if permission has been granted

--- components/ImageManager.tsx
+++ components/ImageManager.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Alert } from 'react-native'
 import React from 'react'
 
 import * as ImagePicker from 'expo-image-picker';
@@ -6,21 +6,33 @@ import * as ImagePicker from 'expo-image-picker';
 export const ImageManager = () => {
     const [response, requestPermission] = ImagePicker.useCameraPermissions();
 
+
+const verifyImagePermissions = async () => {
+    if (response?.granted === false) {
+        await requestPermission();
+        return false;
+    } else {
+        return true;
+    }
+  }
+  
+  const takeImageHandler = async () => {
+    try {
+    if(await verifyImagePermissions()){
+      const result = await ImagePicker.launchCameraAsync({allowsEditing: true, aspect: [16, 9], quality: 0.5});
+      console.log(result);
+        }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  };
+  
     return (
     <View>
        <Button title="Take Image" onPress={()=>{takeImageHandler()}} />
     </View>
     );
 }
-
-const takeImageHandler = async () => {
-  try {
-    const result = await ImagePicker.launchCameraAsync({allowsEditing: true, aspect: [16, 9], quality: 0.5});
-    console.log(result);
-  }
-  catch (err) {
-    console.log(err);
-  }
-};
 
 export default ImageManager
