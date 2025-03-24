ef3ae767c58267ba4bf47a6760b0e1811e313583
Ronald <mundell.r@northeastern.edu>
2025-03-24 13:15:59 -0700

    added permission handling and stores location object

--- components/LocationManager.tsx
+++ components/LocationManager.tsx
@@ -3,16 +3,28 @@ import React, { useState } from 'react'
 import * as Location from 'expo-location';
 
 const LocationManager = () => {
+    const [response, requestPermission] = Location.useForegroundPermissions();
     const [mylocation, setLocation] = useState<Location.LocationObjectCoords>();
+
     const locateUserHandler = async () => {
+        await verifyPermissions();
+        if(response?.granted){
         try {
-            const location = await Location.getCurrentPositionAsync({});
+            const location = await Location.getCurrentPositionAsync();
             setLocation(location.coords);
         }
         catch (err) {
-
+            console.log(err);
+        }
         }
       };
+      
+      const verifyPermissions = async () => {
+        if (response?.granted) return true;
+            
+        const result = await requestPermission();
+        return result.granted;
+      }
 
   return (
     <View>
